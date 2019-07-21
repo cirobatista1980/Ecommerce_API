1438cfbe012cb7848cdaafad2ed8ba663618b804
Ciro Batista <ciro.batista.1980@gmail.com>
2019-07-21 02:55:06 -0300

    Implementado Login

--- EcommerceApi.Core/Services/UsuarioService.cs
+++ EcommerceApi.Core/Services/UsuarioService.cs
@@ -1,5 +1,6 @@
 ﻿using EcommerceApi.Core.Entities;
 using EcommerceApi.Core.Interfaces;
+using EcommerceApi.Core.Entities.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,21 @@ namespace EcommerceApi.Core.Services
 {
     public class UsuarioService : IUsuarioService
     {
+        private readonly IRepositoryBase<Usuario> repository;
+        public UsuarioService(IRepositoryBase<Usuario> _repository)
+        {
+            repository = _repository;
+        }
+
         public bool Login(Usuario usuario)
         {
-            throw new NotImplementedException();
+            if (usuario.IsValid())
+            {
+                var user = repository.GetById(usuario);
+                return (user != null);
+            }
+            else
+                return false;
         }
 
         public bool Logout(Usuario usuario)
--- EcommerceApi.Test/UsuarioServiceTest.cs
+++ EcommerceApi.Test/UsuarioServiceTest.cs
@@ -3,6 +3,7 @@ using EcommerceApi.Core.Interfaces;
 using EcommerceApi.Core.Entities.Extensions;
 using NUnit.Framework;
 using EcommerceApi.Core.Services;
+using Moq;
 
 namespace EcommerceApi.Test
 {
@@ -10,21 +11,51 @@ namespace EcommerceApi.Test
     public class UsuarioServiceTest
     {
         private readonly IUsuarioService service;
+        private readonly IRepositoryBase<Usuario> repository;
+        private readonly Usuario usuarioValido;
+        private readonly Usuario usuarioInValido;
         public UsuarioServiceTest()
         {
-            service = new UsuarioService();
+            usuarioInValido = new Usuario();
+
+            usuarioValido = new Usuario()
+            {
+                Codigo = 0,
+                Email = "usuario@usuario.com",
+                Login = "user",
+                Nome = "user",
+                Senha = "12345678"
+            };
+
+            Mock<IRepositoryBase<Usuario>> mockRepository = new Mock<IRepositoryBase<Usuario>>();
+            mockRepository.Setup(x => x.GetById(usuarioValido)).Returns(usuarioValido);
+            repository = mockRepository.Object;
+            service = new UsuarioService(repository);
+        }
+
+
+
+        [Test]
+        public void UsuarioEhValido()
+        {
+            Assert.IsTrue(usuarioValido.IsValid());
         }
 
         [Test]
-        public void ValidarUsuario()
+        public void UsuarioEhInValido()
         {
-            Usuario user = new Usuario();
-            user.Email = "user@user.com.br";
-            user.Login = "user";
-            user.Nome = "Usuario";
-            user.Senha = "12345678";
+            Assert.IsFalse(usuarioInValido.IsValid());
+        }
 
-            Assert.IsTrue(user.IsValid());
+        [Test]
+        public void LogarUsuarioValido()
+        {
+            Assert.IsTrue(service.Login(usuarioValido));
+        }
+        [Test]
+        public void LogarUsuarioInValido()
+        {
+            Assert.IsFalse(service.Login(usuarioInValido));
         }
     }
 }
